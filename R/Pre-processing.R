029e8d90959a15185b8ad983a7228bc0fe0884f4
ClairBee <clair.with.no.e@gmail.com>
2016-02-12 12:13:03 +0000

    Update Pre-processing.R
    Filenm now defined inside 'for' loop

--- R/Pre-processing.R
+++ R/Pre-processing.R
@@ -16,6 +16,7 @@
 #' 
 load.daily <- function(img.date, channel, x = 1996, y = 1996, z = 20, progress = F, path = "./Image-data/") {
     
+    # convert date to filenm format, set up array
     img.date <- toString(img.date)
     rev.date <- paste(substr(img.date,5,6),substr(img.date,3,4),substr(img.date,1,2), sep = "")
     m <- array(dim = c(x, y, z))
@@ -23,9 +24,10 @@ load.daily <- function(img.date, channel, x = 1996, y = 1996, z = 20, progress =
     # show progress bar or not?
     if (progress) {pb <- txtProgressBar(max = z, style = 3)}
     
-    filenm <- paste(path,img.date,"/",channel,"/",channel,"_",rev.date,"_",i,".tif", sep = "")
-    
+    # read each file in turn, assign values into array
     for (i in 1:z) {
+        filenm <- paste(path,img.date,"/",channel,"/",channel,"_",rev.date,"_",i,".tif", sep = "")
+    
         m[,,i] <- readTIFF(filenm)
         if (progress) {setTxtProgressBar(pb,i)}
     } 
