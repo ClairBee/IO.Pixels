e10eff15420cd68daf3b509c4c52848ee5a3b0ec
ClairBee <clair.with.no.e@gmail.com>
2016-02-12 11:44:22 +0000

    16.02.12 load.daily

---
+++ R/Pre-processing.R
@@ -0,0 +1,33 @@
+
+#' Load Daily Images
+#'
+#' Import one day's images into a single array
+#' @param img.date Date of images to import, in format yymmdd.
+#' @param channel Specify channel to import: black, grey or white.
+#' @param x Width of image. Default is 1996.
+#' @param y Height of image. Default is 1996.
+#' @param z Number of images to import. Default is 20.
+#' @param progress Display progress bar? T or F (default is F)
+#' @param path Path to top level of stored images. Default is "./Image-data/"
+#' @export
+#' @examples
+#' daily <- load.daily(150828, "black")
+#' 
+#' 
+load.daily <- function(img.date, channel, x = 1996, y = 1996, z = 20, progress = F, path = "./Image-data/") {
+    
+    img.date <- toString(img.date)
+    rev.date <- paste(substr(img.date,5,6),substr(img.date,3,4),substr(img.date,1,2), sep = "")
+    m <- array(dim = c(x, y, z))
+    
+    # show progress bar or not?
+    if (progress) {pb <- txtProgressBar(max = z, style = 3)}
+    
+    filenm <- paste(path,img.date,"/",channel,"/",channel,"_",rev.date,"_",i,".tif", sep = "")
+    
+    for (i in 1:z) {
+        m[,,i] <- readTIFF(filenm)
+        if (progress) {setTxtProgressBar(pb,i)}
+    } 
+    return(m)
+}
