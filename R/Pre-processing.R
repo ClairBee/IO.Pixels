b5fbe4b477a89196a7933b0918ba05fc290bb7c1
ClairBee <clair.with.no.e@gmail.com>
2016-02-17 18:29:25 +0000

    15.02.17: functions added to import and summarise xml data when loading an image

--- R/Pre-processing.R
+++ R/Pre-processing.R
@@ -10,34 +10,146 @@
 #' @param fpath Path to top level of stored images. Default is "./Image-data/"
 #' @export
 #' @examples
-#' acq <- load.acq(150828, "black")
+#' b.150828 <- load.image.batch(150828, "black")
 #' 
 #' 
-load.acq <- function(img.date, channel, x = 1996, y = 1996, z = 20, progress = F, fpath = "./Image-data/") {
+load.image.batch <- function(img.date, channel, fpath = "./Image-data/") {
     
     # convert date to filenm format, set up array
     img.date <- toString(img.date)
     rev.date <- paste(substr(img.date,5,6),substr(img.date,3,4),substr(img.date,1,2), sep = "")
+    
+    # check if xml summ already in df? Currently always overwrites
+    xml.data <- load.xml.batch(img.date, channel, fpath)
+    x <- min(xml.data$x.dim)
+    y <- min(xml.data$y.dim)
+    z <- nrow(xml.data)
+            
+    fpath <- paste(fpath, img.date, "/", channel, "/", sep = "")
+    files <- list.files(fpath, pattern = "\\.tif$")
+    
     m <- array(dim = c(x, y, z))
     
-    # show progress bar or not?
-    if (progress) {pb <- txtProgressBar(max = z, style = 3)}
-    
     # read each file in turn, assign values into array
-    for (i in 1:z) {
-        filenm <- paste(fpath,img.date,"/",channel,"/",channel,"_",rev.date,"_",i,".tif", sep = "")
+    for (i in 1:length(files)) {
+        filenm <- paste(fpath,channel,"_",rev.date,"_",i,".tif", sep = "")
         tmp <- readTIFF(filenm)
-        if (length(dim(tmp)) == 2) {
-            tmp <- tmp
-        } else {
-            tmp <- tmp[,,1]
+        if (length(dim(tmp)) > 2) {
+            tmp <- tmp[,,rep(1,length(dim(tmp))-2)]
         }
+
         # flip & transpose matrix to match orientation of original image
         m[,,i] <- t(tmp[nrow(tmp):1,,drop=FALSE])
-        if (progress) {setTxtProgressBar(pb,i)}
-    } 
+    }
+    
+    # overwrite df containing file summary
+    store.batch.data(img.date, channel, xml.data, m)
     return(m)
 }
+
+
+#' Load xml profiles of one day's acquisitions from one channel
+#'
+#' Support function: import xml profiles of one day's images into a single data frame
+#' @param img.date Date of images to import, in format yymmdd.
+#' @param channel Specify channel to import: black, grey or white.
+#' @param fpath Path to top level of stored images. Default is "./Image-data/"
+#' @export
+#' @examples
+#' b.150828.xml <- load.xml.batch(150828, "black")
+#' 
+#' 
+load.xml.batch <- function(img.date, channel, fpath = "./Image-data/") {
+    
+    img.date <- toString(img.date)
+    
+    fpath <- paste(fpath, img.date, "/", channel, "/", sep = "")
+    files <- list.files(fpath, pattern = "\\.xml$")
+    
+    xml.data <- data.frame(acq.time = character(),
+                           x.offset = double(),
+                           y.offset = double(),
+                           x.dim = double(),
+                           y.dim = double(),
+                           stringsAsFactors = F)
+    
+    for (i in 1:length(files)) {
+        tif.profile <- xmlToList(xmlParse(paste(fpath, files[i], sep = "")))
+        xml.data[i,1] <- tif.profile$Time
+        xml.data[i,c(2:5)] <- c(as.integer(tif.profile$CameraProperties$imageOffsetX),
+                                as.integer(tif.profile$CameraProperties$imageOffsetY),
+                                as.integer(tif.profile$CameraProperties$imageSizeX),
+                                as.integer(tif.profile$CameraProperties$imageSizeY))
+    }
+    xml.data <- xml.data[order(xml.data$acq.time),]
+    row.names(xml.data) <- c(1:length(files))
+    return(xml.data)
+}
+
+
+#' Store summary of image batch
+#'
+#' Support function: store key summaries of each day's images in a data frame
+#' @param img.date Date of images to import, in format yymmdd.
+#' @param channel Specify channel to import: black, grey or white.
+#' @param xml.data Output from \link{load.xml.batch} containing tiff profile summaries
+#' @param image.data Matrix of imported tiff images
+#' @export
+#' @examples
+#' store.batch.data(150828, "black", b.150828.xml, b.150828)
+#' 
+#' 
+store.batch.data <- function(img.date, channel, xml.data, image.data) {
+    
+    img.date <- toString(img.date)
+    obj.nm <- paste(substr(channel,1,1),".", img.date, sep = "") 
+    
+    # create df to hold summary, if not already existing
+    if (!exists("img.summ")) {
+        img.summ <<- data.frame(obj.nm = character(),
+                                acq.date = character(),
+                                acq.time = character(),
+                                channel = character(),
+                                mean = numeric(),
+                                sd = numeric(),
+                                x.offset = double(),
+                                y.offset = double(),
+                                x.dim = double(),
+                                y.dim = double(),
+                                frames = double(),
+                                stringsAsFactors = F)
+        r <- 1                                                     # first row
+    } else {
+        if (obj.nm %in% row.names(img.summ)) {
+            r <- which(row.names(img.summ) == obj.nm, arr.ind = T)     # overwrite existing
+        } else {
+            r <- nrow(img.summ) + 1                                # next available row
+        }
+    }
+    
+    # update row r of df with summary information
+    
+    img.summ[r, 1:4] <<- c(obj.nm, img.date, xml.data$acq.time[1], channel)
+    
+    img.summ$mean[r] <<- mean(image.data)
+    img.summ$sd[r] <<- sd(image.data)
+    
+    img.summ$frames[r] <<- nrow(xml.data)
+    
+    # check that offset & dimensions are same in all images
+    if (max(xml.data$x.offset) == min(xml.data$x.offset)) {
+        img.summ$x.offset[r] <<- min(xml.data$x.offset)}
+    
+    if (max(xml.data$y.offset) == min(xml.data$y.offset)) {
+        img.summ$y.offset[r] <<- min(xml.data$y.offset)}
+    
+    if (max(xml.data$x.dim) == min(xml.data$x.dim)) {
+        img.summ$x.dim[r] <<- min(xml.data$x.dim)}
+    
+    if (max(xml.data$y.dim) == min(xml.data$y.dim)) {
+        img.summ$y.dim[r] <<- min(xml.data$y.dim)}
+}
+
 
 
 #' Count available images
