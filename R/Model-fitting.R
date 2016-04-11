8bd547ff7930859639dec04419d1845ae1bcc359
ClairBee <clair.with.no.e@gmail.com>
2016-04-11 16:33:48 +0100

    16.04.11: new function to fit hyper-elliptical spot (ellipse or squircle)

--- R/Model-fitting.R
+++ R/Model-fitting.R
@@ -15,8 +15,10 @@
 #' 
 spot.lm <- function(image, order = 2, centre = c(1023.5, 992.5), robust = F) {
     
-    # get distances from spot centre
-    z.dist <- data.frame(x = c(1:1996^2) %/% 1996 + 1, y = c(1:1996^2) %% 1996)
+    # get distances from spot centre 
+    z.dist <- data.frame(x =  c(1:1996^2)%%1996, y = c(1:1996^2)%/%1996 + 1)
+        z.dist$x[z.dist$x == 0] <- 1996                                 # correct x-coord
+        z.dist$y[z.dist$x == 1996] <- z.dist$y[z.dist$x == 1996] - 1    # correct y-coord
     z.dist$z <- sqrt((z.dist$x - centre[1])^2 + (z.dist$y - centre[2])^2)
     
     # distance from centre vs pixel values
@@ -40,6 +42,67 @@ spot.lm <- function(image, order = 2, centre = c(1023.5, 992.5), robust = F) {
 }
 
 
+#' Fit hyper- or hypo-elliptical spot model
+#' 
+#' Uses linear regression over x- and y-distance from centre to the power n to fit a hyper- or hypo-elliptical spot
+#' @param image Single-layer (1996x1996) array image to be fitted.
+#' @param n Power to which x- and y- distances are to be raised to fit model.
+#' @param order Order of polynomial to include. Default is 2
+#' @param centre Vector of coordinates of centre of spot. Default is centre of uncropped detector, c(1023.5, 992.5)
+#' @param robust Boolean: use robust model fitting (\link{\code{rlm}})? Default is F (use \link{\code{lm}} for regression).
+#' @return Linear model fitted to given image
+#' @details 1 < n < 2: rhombus with convex edges; n = 2: an ellipse; n > 2: rectangle with rounded corners.
+#' @export
+#' @examples
+#' spot <- spot.lm(pw.m[,,"white", "150828"], n = 2, robust = T)
+#' spot.res <- matrix(spot$residuals, ncol = 1996)
+#' pixel.image(spot.res)
+he.spot.lm <- function(image, n = 2, order = 2, centre = c(1023.5, 992.5), robust = F) {
+    
+    # get x & y coords from spot centre
+    z.dist <- data.frame(x =  c(1:1996^2)%%1996, y = c(1:1996^2)%/%1996 + 1)
+    z.dist$x[z.dist$x == 0] <- 1996
+    z.dist$y[z.dist$x == 1996] <- z.dist$y[z.dist$x == 1996] - 1
+    
+    zz <- cbind(melt(image), z.dist,
+                xx = abs(z.dist$x - centre[1])^n, 
+                yy = abs(z.dist$y - centre[2])^n)[, c(4,5,6,7, 3)]
+    
+    # fit & return linear model
+    if (order > 1) {
+        if (robust) {
+            mod <- rlm(value ~ poly(xx, order) + poly(yy, order), zz)
+        } else {
+            mod <- lm(value ~ poly(xx, order) + poly(yy, order), zz)
+        }
+    } else {
+        if (robust) {
+            mod <- rlm(value ~ xx + yy, zz)
+        } else {
+            mod <- lm(value ~ xx + yy, zz)
+        }
+    }
+    return(mod)
+}
+
+
+#' Linear regression over distance from centre
+#' 
+#' Fit a circular spot model with linear gradients to the image, including polynomial terms of z as explanatory variables
+#' @param image Single-layer (1996x1996) array image to be fitted.
+#' @param order Order of polynomial to include. Default is 2
+#' @param centre Vector of coordinates of centre of spot. Default is centre of uncropped detector, c(1023.5, 992.5)
+#' @param robust Boolean: use robust model fitting (\link{\code{rlm}})? Default is F (use \link{\code{lm}} for regression).
+#' @return Linear model fitted to given image
+#' @export
+#' @examples
+#' circ.lm <- spot.lm(pw.m)
+#' circ.res <- matrix(circ.lm$residuals, ncol = 1996)
+#' pixel.image(circ.res)
+#' 
+ 
+
+
 #' Per-panel linear regression
 #' 
 #' Fit a linear gradient over each of the 32 subpanels within the image
@@ -53,7 +116,7 @@ spot.lm <- function(image, order = 2, centre = c(1023.5, 992.5), robust = F) {
 #' panel.lm <- panel.lm(circ.res, "y")                 # finds coefficients of intercept and y.
 #' panel.res <- circ.res - panel.lm$fitted.values
 #' pixel.image(panel.res)
-panel.lm <- function (image, terms = "x + y", robust = F) {
+panel.lm <- function (image, terms = "x + y", robust = F, left.pad = 2, upper.pad = 20) {
     
     # convert formula string to lower case to ensure match
     terms <- tolower(terms)
@@ -78,7 +141,7 @@ panel.lm <- function (image, terms = "x + y", robust = F) {
         coeffs <- rbind(coeffs, coef(lm))
         smoothed.panels[, , s] <- predict(lm, df)
     }
-    list(formula = paste0("value ~ ", terms), fitted.values = join.panels(smoothed.panels), models = coeffs)
+    list(formula = paste0("value ~ ", terms), fitted.values = join.panels(smoothed.panels, left.pad, upper.pad), models = coeffs)
 }
 
 
