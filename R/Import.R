a8bfba391cf3229c2a69a81604da45b188c1b2e6
ClairBee <clair.with.no.e@gmail.com>
2016-06-29 19:01:06 +0100

    16.06.29: functions to import image as 2048 x 2048 padded array

--- R/Import.R
+++ R/Import.R
@@ -355,3 +355,74 @@ load.pixel.sds <- function(fpath = "./Other-data/") {
     # assign in global environment
     pw.sd <<- pw.sd
 }
+
+
+#' Load pixel means into 2048 x 2048 array
+#' 
+#' Load all pixelwise mean summaries into a single labelled array of size 2048 x 2048 (detector size). Cropped pixels are labelled as NA.
+#' @export
+#' 
+load.pixel.means.2048 <- function(fpath = "./02_Objects/images") {
+    ll <- list.files("./02_Objects/images", pattern = "pwm-[0-9]+\\.rds$", full.names = T)
+    
+    pw.m <<- abind(lapply(ll, readRDS), along = 4)
+    dimnames(pw.m)[[4]] <<- unlist(lapply(ll, substring, 25, 30))
+}
+
+
+#' Import a single day's acquisition into a 2048 x 2048 array
+#' 
+#' Import all images for one acquisition date into a 2048 x 2048 array, with layers corresponding to different power settings. Cropped pixels are padded with NA.
+#' @export
+#' @examples
+#' im.160430 <- import.acq("/home/clair/Documents/Pixels/Image-data/160430")
+#' 
+import.acq <- function(acq.folder, subfolders = c("black", "grey", "white"), panel.dim = c(2048, 2048)) {
+    
+    acq <- array(dim = c(panel.dim, length(subfolders)),
+                 dimnames = list(NULL, NULL, subfolders))
+    
+    for (sf in subfolders) {
+        
+        # load xml data into data frame (get image list & offsets)
+        im.list <- list.files(paste0(acq.folder, "/", sf), pattern = "\\.xml$", full.names = T)
+        df <- rbind.fill(lapply(im.list, 
+                                function(im.xml) as.data.frame(lapply(do.call("c", c(xmlToList(xmlParse(im.xml)), list(recursive = TRUE))), 
+                                                                      FUN = unlist), stringsAsFactors = F)))
+        # gives ordered list of images - may be needed if investigating as time series
+        df <- data.frame(acq.time = df$Time,
+                         filenm = df$.attrs.FileName,
+                         offset.x = df$CameraProperties.imageOffsetX,
+                         offset.y = df$CameraProperties.imageOffsetY,
+                         size.x = df$CameraProperties.imageSizeX,
+                         size.y = df$CameraProperties.imageSizeY,
+                         kV = df$kV,
+                         uA = df$uA,
+                         exp.time = df$ExposureTime,
+                         stringsAsFactors = F)[order(acq.time = df$Time),]
+        
+        # quick error check - are all images on same area of detector (should always be T)
+        if (!identical(sapply(df[3:6], min), sapply(df[3:6], max))) {
+            cat("Error - TIF images do not all have same offset & dimension")
+            return(df)
+        }
+        
+        # if all images cover same area of detector, continue
+        offset.x <- as.integer(df$offset.x[1])
+        offset.y <- as.integer(df$offset.y[1])
+        size.x <- as.integer(df$size.x[1])
+        size.y <- as.integer(df$size.y[1])
+        
+        ims <- apply(cbind(acq.folder, "/", sf, "/", df$filenm), 1, paste, collapse = "")
+        
+        # get pixelwise mean of image
+        tmp <- apply(abind(lapply(ims, readTIFF, as.is = T), along = 3), 1:2, mean)
+        tmp <- t(tmp[nrow(tmp):1, , drop = FALSE])
+        
+        # position correctly in array
+        acq[(offset.x + 1) : (offset.x+size.x),
+            (panel.dim[2]-size.y-offset.y + 1) : (panel.dim[2]-offset.y),
+            sf] <- tmp
+    }
+    return(acq)
+}
